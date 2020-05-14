823161c35030540e16c07ca770ebc4787a55510b
bix <840573+bix6@users.noreply.github.com>
2020-05-14 07:26:44 -0700

    update API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `https://radiant-hollows-02668.herokuapp.com/`,
+  API_ENDPOINT: `https://radiant-hollows-02668.herokuapp.com/api/bookmarks`,
   API_KEY: process.env.REACT_APP_API_KEY
 }
